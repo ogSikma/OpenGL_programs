ce7e6e9dc2c4a0b4edecc856338a9ca44940a80c
Marek Pietrowicz <mojkero@gmail.com>
2025-07-09 04:28:30 +0200

    speeding the spehere and stopping after time

---
+++ OpenGL_sphere_in_motion/Zad2/main.cpp
@@ -0,0 +1,543 @@
+﻿#include <windows.h>
+#include <gl\gl.h>
+#include <gl\glu.h>
+#include <gl\glaux.h>
+#include <string>
+
+HDC			hDC = NULL;
+HGLRC		hRC = NULL;
+HWND		hWnd = NULL;
+HINSTANCE	hInstance;
+bool	keys[256];
+bool	active = TRUE;
+bool	fullscreen = false;
+
+
+GLfloat dystansKamery = -55.0f;
+GLfloat kamera_katx = 25.0f;
+GLfloat kamera_katy = 0.0f;
+
+bool przyspieszenie_kuli = true;
+GLfloat x_kuli = -3.0f;
+GLfloat y_kuli = 8.0f;
+
+GLfloat predk_x = 0.0f;
+GLfloat predk_y = 0.0f;
+GLfloat a_x = -0.008f;
+GLfloat a_y = -0.05f;
+
+GLfloat predkoscObrotu = 2.0f;
+
+GLfloat ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+GLfloat diffuseLight[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+GLfloat lightPosition[] = { -5.0f, 0.0f, -5.0f, -1.0f };
+GLfloat ambientMaterial[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+GLfloat diffuseMaterial[] = { 0.4f, 0.8f, 0.5f, 1.0f };
+
+LRESULT	CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);	// Declaration For WndProc
+
+void Rzutnia(GLsizei width, GLsizei height)
+{
+	if (height == 0)						
+	{
+		height = 1;
+	}
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+int Init(GLvoid)										
+{
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+	glEnable(GL_DEPTH_TEST);
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, ambientMaterial);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuseMaterial);
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);	
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);	
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_COLOR_MATERIAL);
+	return TRUE;
+}
+
+int Display(GLvoid)									
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glTranslatef(0.0f, 0.0f, dystansKamery);
+	glRotatef(kamera_katx, 1.0f, 0.0f, 0.0f);
+	glRotatef(kamera_katy, 0.0f, 1.0f, 0.0f);
+
+	glPushMatrix();
+	glTranslatef(0.0f, -0.25f, 0.0f);
+	glColor3f(0.2f, 0.5f, 0.1f);
+	auxSolidBox(48, 0.5, 24);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-20.0f, 0.0f, 0.0f);
+	glTranslatef(0.0f, 4.0f, 0.0f);	
+	glColor3f(1.0f, 0.5f, 0.0f);
+	auxSolidCube(8);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-20.0f, 0.0f, 0.0f);
+	glTranslatef(x_kuli, y_kuli + 2, 0);
+	glColor3f(0.2f, 0.2f, 0.6f);
+	auxSolidSphere(2);
+	glPopMatrix();
+
+	return TRUE;
+}
+
+void BelowZeroY(GLvoid)
+{
+	if (y_kuli < 0)
+	{
+		y_kuli = 0;
+	}
+}
+
+void Movement(GLvoid) {
+	if (przyspieszenie_kuli)
+	{
+		if (x_kuli > 4) przyspieszenie_kuli = false;
+	}
+	else
+	{
+		predk_x = predk_x + a_x;
+		if (predk_x < 0.01)
+		{
+			predk_x = 0;
+			a_x = 0;
+		}
+
+		if (y_kuli == 0)
+		{
+			predk_y *= -0.8;
+			predk_x *= 0.8;
+			if (predk_y < 0.01) 
+			{
+				predk_y = 0;
+			}
+		}
+		predk_y = predk_y + a_y;
+	}
+	x_kuli = x_kuli + predk_x;
+	y_kuli = y_kuli + predk_y;
+	BelowZeroY();
+}
+
+GLvoid KillGLWindow(GLvoid)								// Properly Kill The Window
+{
+	if (fullscreen)										// Are We In Fullscreen Mode?
+	{
+		ChangeDisplaySettings(NULL, 0);					// If So Switch Back To The Desktop
+		ShowCursor(TRUE);								// Show Mouse Pointer
+	}
+
+	if (hRC)											// Do We Have A Rendering Context?
+	{
+		if (!wglMakeCurrent(NULL, NULL))					// Are We Able To Release The DC And RC Contexts?
+		{
+			MessageBox(NULL, "Release Of DC And RC Failed.", "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+		}
+
+		if (!wglDeleteContext(hRC))						// Are We Able To Delete The RC?
+		{
+			MessageBox(NULL, "Release Rendering Context Failed.", "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+		}
+		hRC = NULL;										// Set RC To NULL
+	}
+
+	if (hDC && !ReleaseDC(hWnd, hDC))					// Are We Able To Release The DC
+	{
+		MessageBox(NULL, "Release Device Context Failed.", "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+		hDC = NULL;										// Set DC To NULL
+	}
+
+	if (hWnd && !DestroyWindow(hWnd))					// Are We Able To Destroy The Window?
+	{
+		MessageBox(NULL, "Could Not Release hWnd.", "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+		hWnd = NULL;										// Set hWnd To NULL
+	}
+
+	if (!UnregisterClass("OpenGL", hInstance))			// Are We Able To Unregister Class
+	{
+		MessageBox(NULL, "Could Not Unregister Class.", "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+		hInstance = NULL;									// Set hInstance To NULL
+	}
+}
+
+/*	This Code Creates Our OpenGL Window.  Parameters Are:					*
+ *	title			- Title To Appear At The Top Of The Window				*
+ *	width			- Width Of The GL Window Or Fullscreen Mode				*
+ *	height			- Height Of The GL Window Or Fullscreen Mode			*
+ *	bits			- Number Of Bits To Use For Color (8/16/24/32)			*
+ *	fullscreenflag	- Use Fullscreen Mode (TRUE) Or Windowed Mode (FALSE)	*/
+
+BOOL CreateGLWindow(char* title, int width, int height, int bits, bool fullscreenflag)
+{
+	GLuint		PixelFormat;			// Holds The Results After Searching For A Match
+	WNDCLASS	wc;						// Windows Class Structure
+	DWORD		dwExStyle;				// Window Extended Style
+	DWORD		dwStyle;				// Window Style
+	RECT		WindowRect;				// Grabs Rectangle Upper Left / Lower Right Values
+	WindowRect.left = (long)0;			// Set Left Value To 0
+	WindowRect.right = (long)width;		// Set Right Value To Requested Width
+	WindowRect.top = (long)0;				// Set Top Value To 0
+	WindowRect.bottom = (long)height;		// Set Bottom Value To Requested Height
+
+	fullscreen = fullscreenflag;			// Set The Global Fullscreen Flag
+
+	hInstance = GetModuleHandle(NULL);				// Grab An Instance For Our Window
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;	// Redraw On Size, And Own DC For Window.
+	wc.lpfnWndProc = (WNDPROC)WndProc;					// WndProc Handles Messages
+	wc.cbClsExtra = 0;									// No Extra Window Data
+	wc.cbWndExtra = 0;									// No Extra Window Data
+	wc.hInstance = hInstance;							// Set The Instance
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);			// Load The Default Icon
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);			// Load The Arrow Pointer
+	wc.hbrBackground = NULL;									// No Background Required For GL
+	wc.lpszMenuName = NULL;									// We Don't Want A Menu
+	wc.lpszClassName = "OpenGL";								// Set The Class Name
+
+	if (!RegisterClass(&wc))									// Attempt To Register The Window Class
+	{
+		MessageBox(NULL, "Failed To Register The Window Class.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;											// Return FALSE
+	}
+
+	if (fullscreen)												// Attempt Fullscreen Mode?
+	{
+		DEVMODE dmScreenSettings;								// Device Mode
+		memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));	// Makes Sure Memory's Cleared
+		dmScreenSettings.dmSize = sizeof(dmScreenSettings);		// Size Of The Devmode Structure
+		dmScreenSettings.dmPelsWidth = width;				// Selected Screen Width
+		dmScreenSettings.dmPelsHeight = height;				// Selected Screen Height
+		dmScreenSettings.dmBitsPerPel = bits;					// Selected Bits Per Pixel
+		dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+		// Try To Set Selected Mode And Get Results.  NOTE: CDS_FULLSCREEN Gets Rid Of Start Bar.
+		if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
+		{
+			// If The Mode Fails, Offer Two Options.  Quit Or Use Windowed Mode.
+			if (MessageBox(NULL, "The Requested Fullscreen Mode Is Not Supported By\nYour Video Card. Use Windowed Mode Instead?", "NeHe GL", MB_YESNO | MB_ICONEXCLAMATION) == IDYES)
+			{
+				fullscreen = FALSE;		// Windowed Mode Selected.  Fullscreen = FALSE
+			}
+			else
+			{
+				// Pop Up A Message Box Letting User Know The Program Is Closing.
+				MessageBox(NULL, "Program Will Now Close.", "ERROR", MB_OK | MB_ICONSTOP);
+				return FALSE;									// Return FALSE
+			}
+		}
+	}
+
+	if (fullscreen)												// Are We Still In Fullscreen Mode?
+	{
+		dwExStyle = WS_EX_APPWINDOW;								// Window Extended Style
+		dwStyle = WS_POPUP;										// Windows Style
+		ShowCursor(FALSE);										// Hide Mouse Pointer
+	}
+	else
+	{
+		dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;			// Window Extended Style
+		dwStyle = WS_OVERLAPPEDWINDOW;							// Windows Style
+	}
+
+	AdjustWindowRectEx(&WindowRect, dwStyle, FALSE, dwExStyle);		// Adjust Window To True Requested Size
+
+	// Create The Window
+	if (!(hWnd = CreateWindowEx(dwExStyle,							// Extended Style For The Window
+		"OpenGL",							// Class Name
+		title,								// Window Title
+		dwStyle |							// Defined Window Style
+		WS_CLIPSIBLINGS |					// Required Window Style
+		WS_CLIPCHILDREN,					// Required Window Style
+		0, 0,								// Window Position
+		WindowRect.right - WindowRect.left,	// Calculate Window Width
+		WindowRect.bottom - WindowRect.top,	// Calculate Window Height
+		NULL,								// No Parent Window
+		NULL,								// No Menu
+		hInstance,							// Instance
+		NULL)))								// Dont Pass Anything To WM_CREATE
+	{
+		KillGLWindow();								// Reset The Display
+		MessageBox(NULL, "Window Creation Error.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;								// Return FALSE
+	}
+
+	static	PIXELFORMATDESCRIPTOR pfd =				// pfd Tells Windows How We Want Things To Be
+	{
+		sizeof(PIXELFORMATDESCRIPTOR),				// Size Of This Pixel Format Descriptor
+		1,											// Version Number
+		PFD_DRAW_TO_WINDOW |						// Format Must Support Window
+		PFD_SUPPORT_OPENGL |						// Format Must Support OpenGL
+		PFD_DOUBLEBUFFER,							// Must Support Double Buffering
+		PFD_TYPE_RGBA,								// Request An RGBA Format
+		bits,										// Select Our Color Depth
+		0, 0, 0, 0, 0, 0,							// Color Bits Ignored
+		0,											// No Alpha Buffer
+		0,											// Shift Bit Ignored
+		0,											// No Accumulation Buffer
+		0, 0, 0, 0,									// Accumulation Bits Ignored
+		16,											// 16Bit Z-Buffer (Depth Buffer)  
+		0,											// No Stencil Buffer
+		0,											// No Auxiliary Buffer
+		PFD_MAIN_PLANE,								// Main Drawing Layer
+		0,											// Reserved
+		0, 0, 0										// Layer Masks Ignored
+	};
+
+	if (!(hDC = GetDC(hWnd)))							// Did We Get A Device Context?
+	{
+		KillGLWindow();								// Reset The Display
+		MessageBox(NULL, "Can't Create A GL Device Context.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;								// Return FALSE
+	}
+
+	if (!(PixelFormat = ChoosePixelFormat(hDC, &pfd)))	// Did Windows Find A Matching Pixel Format?
+	{
+		KillGLWindow();								// Reset The Display
+		MessageBox(NULL, "Can't Find A Suitable PixelFormat.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;								// Return FALSE
+	}
+
+	if (!SetPixelFormat(hDC, PixelFormat, &pfd))		// Are We Able To Set The Pixel Format?
+	{
+		KillGLWindow();								// Reset The Display
+		MessageBox(NULL, "Can't Set The PixelFormat.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;								// Return FALSE
+	}
+
+	if (!(hRC = wglCreateContext(hDC)))				// Are We Able To Get A Rendering Context?
+	{
+		KillGLWindow();								// Reset The Display
+		MessageBox(NULL, "Can't Create A GL Rendering Context.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;								// Return FALSE
+	}
+
+	if (!wglMakeCurrent(hDC, hRC))					// Try To Activate The Rendering Context
+	{
+		KillGLWindow();								// Reset The Display
+		MessageBox(NULL, "Can't Activate The GL Rendering Context.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;								// Return FALSE
+	}
+
+	ShowWindow(hWnd, SW_SHOW);						// Show The Window
+	SetForegroundWindow(hWnd);						// Slightly Higher Priority
+	SetFocus(hWnd);									// Sets Keyboard Focus To The Window
+	Rzutnia(width, height);					// Set Up Our Perspective GL Screen
+
+	if (!Init())									// Initialize Our Newly Created GL Window
+	{
+		KillGLWindow();								// Reset The Display
+		MessageBox(NULL, "Initialization Failed.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;								// Return FALSE
+	}
+
+	return TRUE;									// Success
+}
+
+LRESULT CALLBACK WndProc(HWND	hWnd,			// Handle For This Window
+	UINT	uMsg,			// Message For This Window
+	WPARAM	wParam,			// Additional Message Information
+	LPARAM	lParam)			// Additional Message Information
+{
+	switch (uMsg)									// Check For Windows Messages
+	{
+	case WM_ACTIVATE:							// Watch For Window Activate Message
+	{
+		if (!HIWORD(wParam))					// Check Minimization State
+		{
+			active = TRUE;						// Program Is Active
+		}
+		else
+		{
+			active = FALSE;						// Program Is No Longer Active
+		}
+
+		return 0;								// Return To The Message Loop
+	}
+
+	case WM_SYSCOMMAND:							// Intercept System Commands
+	{
+		switch (wParam)							// Check System Calls
+		{
+		case SC_SCREENSAVE:					// Screensaver Trying To Start?
+		case SC_MONITORPOWER:				// Monitor Trying To Enter Powersave?
+			return 0;							// Prevent From Happening
+		}
+		break;									// Exit
+	}
+
+	case WM_CLOSE:								// Did We Receive A Close Message?
+	{
+		PostQuitMessage(0);						// Send A Quit Message
+		return 0;								// Jump Back
+	}
+
+	case WM_KEYDOWN:							// Is A Key Being Held Down?
+	{
+		keys[wParam] = TRUE;					// If So, Mark It As TRUE
+		return 0;								// Jump Back
+	}
+
+	case WM_KEYUP:								// Has A Key Been Released?
+	{
+		keys[wParam] = FALSE;					// If So, Mark It As FALSE
+		return 0;								// Jump Back
+	}
+
+	case WM_SIZE:								// Resize The OpenGL Window
+	{
+		Rzutnia(LOWORD(lParam), HIWORD(lParam));  // LoWord=Width, HiWord=Height
+		return 0;								// Jump Back
+	}
+	}
+
+	// Pass All Unhandled Messages To DefWindowProc
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE	hInstance,			// Instance
+	HINSTANCE	hPrevInstance,		// Previous Instance
+	LPSTR		lpCmdLine,			// Command Line Parameters
+	int			nCmdShow)			// Window Show State
+{
+	MSG		msg;									// Windows Message Structure
+	BOOL	done = FALSE;								// Bool Variable To Exit Loop
+
+	// Create Our OpenGL Window
+	if (!CreateGLWindow("OpenGL zadanie 2 - rzut z odbiciem", 800, 600, 16, fullscreen))
+	{
+		return 0;									// Quit If Window Was Not Created
+	}
+	int ostatniCzas = 0;
+	while (!done)									// Loop That Runs While done=FALSE
+	{
+		bool rendered = false;
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))	// Is There A Message Waiting?
+		{
+			if (msg.message == WM_QUIT)				// Have We Received A Quit Message?
+			{
+				done = TRUE;							// If So done=TRUE
+			}
+			else									// If Not, Deal With Window Messages
+			{
+				TranslateMessage(&msg);				// Translate The Message
+				DispatchMessage(&msg);				// Dispatch The Message
+			}
+		}
+		else										// If There Are No Messages
+		{
+			// Draw The Scene.  Watch For ESC Key And Quit Messages From DrawGLScene()
+			if (active)								// Program Active?
+			{
+				if (keys[VK_ESCAPE])				// Was ESC Pressed?
+				{
+					done = TRUE;						// ESC Signalled A Quit
+				}
+				else
+				{
+					int czas = GetTickCount();
+					if (czas - ostatniCzas >= 20)
+					{
+						Display();
+						Movement();
+						SwapBuffers(hDC);
+						ostatniCzas = czas;
+						rendered = true;
+					}
+				}
+			}
+			if (rendered)
+			{
+				if (keys[VK_F1])						// Is F1 Being Pressed?
+				{
+					keys[VK_F1] = FALSE;					// If So Make Key FALSE
+					KillGLWindow();						// Kill Our Current Window
+					fullscreen = !fullscreen;				// Toggle Fullscreen / Windowed Mode
+					// Recreate Our OpenGL Window
+					if (!CreateGLWindow("OpenGL zadanie 2 - rzut z odbiciem", 800, 600, 16, fullscreen))
+					{
+						return 0;						// Quit If Window Was Not Created
+					}
+				}
+				if (keys[VK_UP])
+				{
+					kamera_katx = kamera_katx + predkoscObrotu;
+					if (kamera_katx > 40) {
+						kamera_katx = 40;
+					}
+				}
+				if (keys[VK_DOWN])
+				{
+					kamera_katx = kamera_katx - predkoscObrotu;
+					if (kamera_katx < 0) {
+						kamera_katx = 0;
+					}
+				}
+				if (keys[VK_RIGHT])	
+				{
+					kamera_katy = kamera_katy + predkoscObrotu;
+					if (kamera_katy > 360) {
+						kamera_katy = kamera_katy - 360;
+					}
+					if (kamera_katy < 0) {
+						kamera_katy = kamera_katy + 360;
+					}
+				}
+				if (keys[VK_LEFT])
+				{
+					kamera_katy = kamera_katy - predkoscObrotu;
+					if (kamera_katy > 360) {
+						kamera_katy = kamera_katy - 360;
+					}
+					if (kamera_katy < 0) {
+						kamera_katy = kamera_katy + 360;
+					}
+				}
+				if (keys['R'])
+				{
+					keys['R'] = false;
+					przyspieszenie_kuli = true;
+					x_kuli = -3.0f;
+					y_kuli = 8.0f;
+					predk_x = 0.0f;
+					predk_y = 0.0f;
+					a_x = -0.008f;
+					a_y = -0.05f;
+				}
+				if (keys['A'])
+				{
+					if (przyspieszenie_kuli) {
+						predk_x = predk_x + 0.05f;
+					}
+				}			
+			}
+		}
+	}
+
+	// Shutdown
+	KillGLWindow();									// Kill The Window
+	return (msg.wParam);							// Exit The Program
+}
+
+
+
+
